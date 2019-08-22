5fa40e2bf777c86deb1942804fc6f544dc2a7a24
ruivo <leonardovieirabfh@gmail.com>
2019-08-22 13:35:03 -0300

    Add produto view

--- appproduto/urls.py
+++ appproduto/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
+from appproduto.views.add_produto_view import add_produto_view
 from appproduto.views.create_produto_view import ProdutoCreateView
 from appproduto.views.delete_produto_view import ProdutoDeleteView
 from appproduto.views.list_produto_view import ProdutoListView
@@ -8,6 +9,7 @@ app_name = 'appproduto'
 
 urlpatterns = [
     path('', ProdutoListView.as_view(), name='produto_list_view'),
+    path('produto/add/pedido/<pk>', add_produto_view, name='produto_add_view'),
     path('produto/deletar/<pk>', ProdutoDeleteView.as_view(), name='produto_delete_view'),
     path('produto/cadastrar', ProdutoCreateView.as_view(), name='produto_create_view'),
 ]
--- appproduto/views/add_produto_view.py
+++ appproduto/views/add_produto_view.py
@@ -1,22 +1,30 @@
 from random import random
 
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 
 from appproduto.models import Produto, Pedido
 
 
-class AddProdutoView(View):
+def add_produto_view(request, pk):
 
-    def get(self, request, id):
-        user = request.user
+    user = request.user
+    user.save()
 
-        produto = get_object_or_404(Produto, id=id)
+    produto1 = Produto.objects.get(pk=pk)
 
-        pedido = Pedido()
-        pedido.user = user
-        pedido.numero_pedido = random.randint(1, 10000)
-        pedido.produto.add(produto)
+    pedido1 = Pedido()
+    pedido1.pk = 1
+    pedido1.numero_pedido = 123
+    pedido1.save()
+
+    pedido1.produto.add(produto1)
+    pedido1.save()
+
+    user.pedido = pedido1
+    user.save()
+
+    return redirect('appproduto:produto_list_view')
 
 
 
